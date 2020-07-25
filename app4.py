cff2ada84b87f30cb640b97f959d83157d8d85ce
Gajju-Bhai <vivekananda0463@gmail.com>
2020-07-25 20:34:08 +0530

    Customized flask-jwt-extended callbacks and responses. Blacklisting few ids to do not have access.

--- app4.py
+++ app4.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 #from flask_jwt import JWT
 from flask_jwt_extended import JWTManager
@@ -9,22 +9,71 @@ from flask_jwt_extended import JWTManager
 from resources.user import UserRegister, User, UserList, UserLogin, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///my_data.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config["JWT_BLACKLIST_ENABLED"] = True
+app.config["JWT_BLACKLIST_TOKEN_CHECKS"] = ["access", "refresh"]
 app.secret_key = "vivek"   # app.config['JWT_SECRET_KEY']
 api = Api(app)
 
 #jwt = JWT(app, authenticate, identity)  # /auth
 jwt = JWTManager(app)
 
+# This method will check if a token is blacklisted, and will be called automatically when blacklist is enabled
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['identity'] in BLACKLIST  # Here we blacklist particular users.
+
 @jwt.user_claims_loader
 def add_claims_to_jwt(identity):
     if identity == 1:    # instead of hard-coding the value, you should read it from a config file or a database
         return {"is_admin": True}
     return {"is_admin": False}
+
+# The following callbacks are used for customizing jwt response/error messages.
+# The original ones may not be in a very pretty format (opinionated)
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({
+        'description': 'The token has expired.',
+        'error': 'token_expired'
+    }), 401
+
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):  # we have to keep the argument here, since it's passed in by the caller internally
+    return jsonify({
+        'description': 'Signature verification failed.',
+        'error': 'invalid_token'
+    }), 401
+
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'description': 'Request does not contain an access token.',
+        'error': 'authorization_required'
+    }), 401
+
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback():
+    return jsonify({
+        'description': 'The token is not fresh.',
+        'error': 'fresh_token_required'
+    }), 401
+
+
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return jsonify({
+        'description': 'The token has been revoked.',
+        'error': 'token_revoked'
+    }), 401
 
 # class Student(Resource):
 #     def get(self,name):
